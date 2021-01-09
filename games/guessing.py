91d93920795c3d8b2f70ec296abdcdd0842df8ce
Felipe Pereira <felipe.pereira@app4point.com.br>
2021-01-09 17:10:07 -0300

    Adicionado o modulo random via import. Alterado loop para utilizar for com range. Usando if com continue e break.

--- games/guessing.py
+++ games/guessing.py
@@ -1,16 +1,22 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
-secret_number = 43
+secret_number = random.randrange(1, 101)
 number_attempts = 3
 roundNumber = 1
 
-while roundNumber <= number_attempts:
+for roundNumber in range(1, number_attempts + 1):
     print("Tentativa {} de {}".format(roundNumber, number_attempts))
     attempt_str = input("Digite o seu numero: ")
     print("Você digitou ", attempt_str)
     attempt = int(attempt_str)
+
+    if attempt < 1 or attempt > 100:
+        print("Você deve digitar um número entre 1 e 100.")
+        continue
 
     isCorrect = attempt == secret_number
     isBigger = attempt > secret_number
@@ -18,6 +24,7 @@ while roundNumber <= number_attempts:
 
     if isCorrect:
         print("Você acertou!")
+        break
     else:
         if isBigger:
             print("Você errou! Valor informado maior que o número secreto.")
