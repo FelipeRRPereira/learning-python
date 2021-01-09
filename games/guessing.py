4e75d66160745b483a1bbd99372c2c92e5f5088e
Felipe Pereira <felipe.pereira@app4point.com.br>
2021-01-09 18:27:03 -0300

    Adicionado niveis dificuldade e calculo de pontos.

--- games/guessing.py
+++ games/guessing.py
@@ -5,8 +5,20 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 secret_number = random.randrange(1, 101)
-number_attempts = 3
-roundNumber = 1
+number_attempts = 0
+score = 1000
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+level = int(input("Defina o nível:"))
+
+if level == 1:
+    number_attempts = 20
+elif level == 2:
+    number_attempts = 10
+else:
+    number_attempts = 5
 
 for roundNumber in range(1, number_attempts + 1):
     print("Tentativa {} de {}".format(roundNumber, number_attempts))
@@ -23,15 +35,16 @@ for roundNumber in range(1, number_attempts + 1):
     isSmaller = attempt < secret_number
 
     if isCorrect:
-        print("Você acertou!")
+        print("Você acertou e fez {:7.2f} pontos!".format(score))
         break
     else:
         if isBigger:
             print("Você errou! Valor informado maior que o número secreto.")
         elif isSmaller:
             print("Você errou! Valor informado menor que o número secreto.")
+        lost_score = abs(secret_number - attempt) / 3
+        score = score - lost_score
 
-    roundNumber = roundNumber + 1
 
 print("************")
 print("Fim do jogo!")
