ddf052e9647f5311bc5d199937ede16f33fb015c
Felipe Pereira <felipe.pereira@app4point.com.br>
2021-01-10 17:04:13 -0300

    Adicionado um novo jogo.

---
+++ games/hangman.py
@@ -0,0 +1,12 @@
+def play():
+    print("*********************************")
+    print("** Bem vindo ao jogo de forca! **")
+    print("*********************************")
+
+    print("************")
+    print("Fim do jogo!")
+    print("************")
+
+
+if __name__ == "__main__":
+    play()
