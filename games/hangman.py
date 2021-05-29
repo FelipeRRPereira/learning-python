4abcc51a8492214fd98b069a05141343c32694ec
Felipe Pereira <felipe.pereira@app4point.com.br>
2021-05-29 02:10:00 -0300

    Finalizando o jogo da forca.

--- games/hangman.py
+++ games/hangman.py
@@ -13,22 +13,22 @@ def play():
     error = 0
 
     while(not hanged and not hit):
-        
         attempt = input_attempt()
 
         if (attempt in secret_word):
             mark_correct_letter(secret_word, attempt, correct_letter)
         else:
             error += 1
+            draw_hangman(error)
 
-        hanged = error == 6
+        hanged = error == 7
         hit = "_" not in correct_letter
         print(correct_letter)
 
     if(hit):
-        print("You Won!!")
+        print_won()
     else:
-        print("Game Over!!")
+        print_game_over(secret_word)
 
     print_finish()
 
@@ -72,6 +72,92 @@ def mark_correct_letter(secret_word, attempt, correct_letter):
         if(attempt == letter):
             correct_letter[index] = attempt
         index += 1
+
+
+def print_game_over(secret_word):
+    print("Ohh no, you were hanged!")
+    print("The secret word was {}".format(secret_word))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def print_won():
+    print("Congratulations, you won!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def draw_hangman(error):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(error == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(error == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(error == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(error == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(error == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(error == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (error == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if __name__ == "__main__":
